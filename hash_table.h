f8e62a7b2b85638bfe216a4adff44cf377019db5
Mohammad Khan <khanmo2k@gmail.com>
2019-11-24 11:25:56 -0500

    Created Map data structure
    In order to implement the Life class, I needed a hash table
    implementation of a map data structure. The one created obeys the
    associative array abstraction, and also takes the key type, value type,
    and hash functor as template parameters. It lacks a method to remove
    key/value entries as it is not needed for the Life class.

---
+++ hash_table.h
@@ -0,0 +1,215 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#include <cassert>
+#include <functional>
+#include <utility>
+
+template <typename Key, typename Val, typename Hash=std::hash<Key>>
+class Map {
+private:
+  // bucket type: empty or filled
+  enum BucketType {
+    EMPTY,
+    FILLED
+  };
+
+  // bucket holds key-value pairs, along with bucket type
+  struct Bucket {
+    BucketType type = BucketType::EMPTY;
+    Key key;
+    Val val;
+  };
+
+  // linked list node holds pointer to a bucket
+  struct Node {
+    Bucket *bucket_ptr;
+    Node *next;
+  };
+
+
+public:
+  // iterator over map elements
+  // 
+  // iterators point to pairs whose 'first' member is the key, and 'second'
+  // member is the value
+  // 
+  // note the value returned is a copy, not a reference
+  class Iterator {
+  private:
+    friend class Map; // let Map access private members
+
+    Node *node_ptr;   // holds a Node *
+
+    Iterator(Node *node_ptr) : node_ptr(node_ptr) { }
+  
+  public:
+    std::pair<Key, Val> operator*() {
+      std::pair<Key, Val> entry(node_ptr->bucket_ptr->key,
+                                node_ptr->bucket_ptr->val);
+      return entry;
+    }
+
+    Iterator & operator++() {
+      node_ptr = node_ptr->next;
+      return *this;
+    }
+
+    bool operator==(Iterator other) const {
+      return node_ptr == other.node_ptr;
+    }
+
+    bool operator!=(Iterator other) const {
+      return node_ptr != other.node_ptr;
+    }
+  };
+
+
+private:
+  // maximum ratio of num_elts to capacity
+  static constexpr const float LOAD_FACTOR = 0.5f;
+
+  std::size_t num_elts; // number of key-value pairs
+
+  std::size_t capacity; // size of buckets dynamic array
+
+  Bucket *buckets;      // dynamic array of buckets
+
+  Hash hasher;          // hash functor
+
+  bool has_list;        // if true, this hash table keeps track of all filled
+                        // buckets in a linked list, and does not otherwise
+
+  Node *first;          // linked list of filled buckets
+
+
+  // REQUIRES: bucket_ptr is a pointer to a valid bucket
+  // EFFECTS: pushes bucket_ptr to the front of the linked list
+  void list_push_front(Bucket *bucket_ptr) {
+    first = new Node{ bucket_ptr, first };
+  }
+
+  // REQUIRES: first points to a valid node
+  // EFFECTS: pops the first element from the linked list
+  void list_pop_front() {
+    // missing assert statement, be extra careful
+    Node *victim = first;
+    first = first->next;
+    delete victim;
+  }
+
+  // EFFECTS: clears the linked list
+  void list_clear() {
+    while (first) {
+      list_pop_front();
+    }
+  }
+
+  // REQUIRES: capacity > 0
+  // EFFECTS: creates a Map with specified capacity
+  Map(std::size_t capacity, bool has_list)
+    : num_elts(0), capacity(capacity), buckets(new Bucket[capacity]),
+      has_list(has_list), first(nullptr) { 
+    assert(capacity > 0);
+  }
+
+  // EFFECTS: returns the index in 'buckets' that the given key hashes to
+  std::size_t hash(Key key) const {
+    return hasher(key) & (capacity - 1); // capacity is a power of 2
+  }
+
+  // EFFECTS: resizes the buckets array to have the new specified capacity
+  void resize(std::size_t new_capacity) {
+    // missing assert statement, be extra careful
+    Map copy(new_capacity, false);
+
+    for (Node *p = first; p; p = p->next) {
+      copy[p->bucket_ptr->key] = p->bucket_ptr->val;
+    }
+
+    capacity = new_capacity;
+
+    Bucket *t = buckets;
+    buckets = copy.buckets;
+    copy.buckets = t;
+  }
+
+
+public:
+  // EFFECTS: creates a map with initial capacity 4
+  Map()
+    : Map(4, true) { }
+
+  // EFFECTS: destructs map
+  ~Map() {
+    delete[] buckets;
+    list_clear();
+  }
+
+  // EFFECTS: returns the number of key-value pairs
+  std::size_t size() const {
+    return num_elts;
+  }
+
+  // EFFECTS: returns true if the given key is contained in the map
+  bool contains(Key key) const {
+    for (std::size_t i = hash(key); buckets[i].type; i = (i + 1) % capacity) {
+      if (buckets[i].key == key) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  // EFFECTS: returns a reference to the value corresponding to the given key.
+  //          if the key isn't in the map, then an entry is created before
+  //          returning the corresponding value.
+  Val & operator[](Key key) {
+    std::size_t i;
+    for (i = hash(key); buckets[i].type; i = (i + 1) % capacity) {
+      if (buckets[i].key == key) {
+        return buckets[i].val;
+      }
+    }
+
+    // grow table if needed
+    if (static_cast<float>(num_elts) / static_cast<float>(capacity)
+        > LOAD_FACTOR) {
+      resize(capacity * 2);
+    }
+
+    // key was not found, insert a new entry
+    if (has_list) {
+      list_push_front(buckets + i);
+    }
+
+    ++num_elts;
+    buckets[i].type = BucketType::FILLED;
+    buckets[i].key = key;
+    return buckets[i].val;
+  }
+
+  // EFFECTS: removes all key-value pairs from the map
+  // NOTE: capacity is not changed by clearing
+  void clear() {
+    while (first) {
+      first->bucket_ptr->type = BucketType::EMPTY;
+      list_pop_front();
+    }
+
+    num_elts = 0;
+  }
+
+  // EFFECTS: returns an iterator pointing to the first entry in the map
+  Iterator begin() const {
+    return Iterator(first);
+  }
+
+  // EFFECTS: returns an iterator pointing off the end of the map
+  Iterator end() const {
+    return Iterator(nullptr);
+  }
+};
+
+#endif
