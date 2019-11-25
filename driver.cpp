74e8f190dccdf5772fd54c58edf6218d167f8751
Mohammad Khan <khanmo2k@gmail.com>
2019-11-25 11:59:04 -0500

    Created initial driver implementation
    The driver program takes one argument, the path to a pattern file in RLE
    format. At program launch, the driver prints to stdout the initial
    configuration of the grid. Then, the user can enter a number to specify
    how many generations to progress by, or a blank line to simply progress
    by 1 generation. The grid configuration after the progression is printed
    to stdout. The user can repeat this process as many times as they want
    to. The program ends upon reaching EOF.

--- driver.cpp
+++ driver.cpp
@@ -1,9 +1,65 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
 #include "Life.h"
 
 using namespace std;
 
-int main() {
+bool is_number(string &s) {
+  for (auto it = s.begin(); it < s.end(); ++it) {
+    if (!isdigit(*it)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+int num_generations(string &input) {
+  if (input == "") {
+    return 1;
+  }
+  else if (is_number(input)) {
+    int n = stoi(input);
+
+    return n > 0 ? n : 0;
+  }
+  else {
+    return 0;
+  }
+}
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    cout << "Usage: ./interactive.exe FILE" << endl;
+    return 1;
+  }
+
+  ifstream fin(argv[1]);
+  if (!fin.is_open()) {
+    cout << "Error: could not open " << argv[1] << endl;
+    return 2;
+  }
+
   Life life;
-  life.add_rle(cin, 0, 0);
+  life.add_rle(fin, 0, 0);
+  cout << "Generation 0" << endl;
+  cout << life << endl;
+
+  string s;
+  int n = 0;
+  while (getline(cin, s)) {
+    int count = num_generations(s);
+
+    if (count > 0) {
+      for (int i = 0; i < count; ++i) {
+        life.progress();
+        ++n;
+      }
+
+      cout << endl;
+      cout << "Generation " << n << endl;
+      cout << life << endl;
+    }
+  }
 }
