062024cd8b72439a190ea7428bdcada9f729743e
Mohammad Khan <khanmo2k@gmail.com>
2019-12-14 20:17:18 -0500

    Refactored driver.cpp to use error handling
    Refactored input parsing code to use error handling when converting
    input to an int with stoi.

--- driver.cpp
+++ driver.cpp
@@ -6,25 +6,15 @@
 
 using namespace std;
 
-bool is_number(string &s) {
-  for (auto it = s.begin(); it < s.end(); ++it) {
-    if (!isdigit(*it)) {
-      return false;
-    }
-  }
-  return true;
-}
-
 int num_generations(string &input) {
-  if (input == "") {
+  if (input.empty()) {
     return 1;
   }
-  else if (is_number(input)) {
-    int n = stoi(input);
 
-    return n > 0 ? n : 0;
+  try {
+    return stoi(input);
   }
-  else {
+  catch (invalid_argument &e) {
     return 0;
   }
 }
